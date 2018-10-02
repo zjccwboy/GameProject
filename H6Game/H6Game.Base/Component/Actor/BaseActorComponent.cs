f765c58a3c60cb86f926eeb7cc6bd70e85bda49d
Sam <70380130@qq.com>
2018-10-03 02:35:31 +0800

    Actor增加辅助消息类，更清晰解耦Actor消息的发布订阅逻辑。

--- H6Game/H6Game.Base/Component/Actor/BaseActorComponent.cs
+++ H6Game/H6Game.Base/Component/Actor/BaseActorComponent.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Threading.Tasks;
+
 namespace H6Game.Base
 {
     public abstract class BaseActorComponent<TEntity> : BaseActorEntityComponent where TEntity : BaseEntity
@@ -40,15 +42,34 @@ namespace H6Game.Base
         public ActorEntity ActorEntity { get; } = new ActorEntity();
         public abstract ActorType ActorType { get;}
         public bool IsLocalActor { get; protected set; }
+    }
 
-        public void SendMySelf(Network network, int messageCmd)
+    public static class ActorHelper
+    {
+        public static void SendMySelf(this BaseActorEntityComponent actor, Network network, int messageCmd)
         {
             var syncMessage = new ActorSyncMessage
             {
-                ObjectId = this.ActorEntity.Id,
-                ActorType = this.ActorEntity.ActorType,
+                ObjectId = actor.ActorEntity.Id,
+                ActorType = actor.ActorEntity.ActorType,
             };
-            network.SendActor(syncMessage, messageCmd, this.Id);
+            network.SendActor(syncMessage, messageCmd, actor.Id);
+        }
+
+        public static void SendActor<TSender>(this BaseActorEntityComponent actor, TSender data, int messageCmd) where TSender : class
+        {
+            if (actor.IsLocalActor)
+                throw new ComponentException("LocalActor不能发送Actor消息，只有RemoteActor才能够跟远程的Actor通讯。");
+
+            actor.ActorEntity.Network.SendActor(data, messageCmd, actor.ActorEntity.ActorId);
+        }
+
+        public static async Task<CallResult<TResponse>> CallActor<TRequest, TResponse>(this BaseActorEntityComponent actor, TRequest request, int messageCmd) where TRequest : class
+        {
+            if (actor.IsLocalActor)
+                throw new ComponentException("LocalActor不能发送Actor消息，只有RemoteActor才能够跟远程的Actor通讯。");
+
+            return await actor.ActorEntity.Network.CallActorAsync<TRequest, TResponse>(request, messageCmd, actor.ActorEntity.ActorId);
         }
     }
 }
