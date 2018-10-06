462d4afc0963251bd2a2d594fb81bdb65984a56a
sam <70380130@qq.com>
2018-10-06 18:10:10 +0700

    MyType Instace 改为private

--- H6Game/H6Game.Core/H6Game.Base/Message/Network/Value.cs
+++ H6Game/H6Game.Core/H6Game.Base/Message/Network/Value.cs
@@ -7,7 +7,7 @@ namespace H6Game.Base
 
     public sealed class MyInt32 : Value<int>
     {
-        protected static MyInt32 Instace { get; } = new MyInt32();
+        private static MyInt32 Instace { get; } = new MyInt32();
 
         public static explicit operator MyInt32(int data)
         {
@@ -23,7 +23,7 @@ namespace H6Game.Base
 
     public sealed class MyUInt32 : Value<uint>
     {
-        protected static MyUInt32 Instace { get; } = new MyUInt32();
+        private static MyUInt32 Instace { get; } = new MyUInt32();
 
         public static explicit operator MyUInt32(uint data)
         {
@@ -39,7 +39,7 @@ namespace H6Game.Base
 
     public sealed class MyLong : Value<long>
     {
-        protected static MyLong Instace { get; } = new MyLong();
+        private static MyLong Instace { get; } = new MyLong();
 
         public static explicit operator MyLong(long data)
         {
@@ -55,7 +55,7 @@ namespace H6Game.Base
 
     public sealed class MyULong : Value<ulong>
     {
-        protected static MyULong Instace { get; } = new MyULong();
+        private static MyULong Instace { get; } = new MyULong();
 
         public static explicit operator MyULong(ulong data)
         {
@@ -71,7 +71,7 @@ namespace H6Game.Base
 
     public sealed class MyFloat : Value<float>
     {
-        protected static MyFloat Instace { get; } = new MyFloat();
+        private static MyFloat Instace { get; } = new MyFloat();
 
         public static explicit operator MyFloat(float data)
         {
@@ -87,7 +87,7 @@ namespace H6Game.Base
 
     public sealed class MyDouble : Value<double>
     {
-        protected static MyDouble Instace { get; } = new MyDouble();
+        private static MyDouble Instace { get; } = new MyDouble();
 
         public static explicit operator MyDouble(double data)
         {
@@ -103,7 +103,7 @@ namespace H6Game.Base
 
     public sealed class MyDecimal : Value<decimal>
     {
-        protected static MyDecimal Instace { get; } = new MyDecimal();
+        private static MyDecimal Instace { get; } = new MyDecimal();
 
         public static explicit operator MyDecimal(decimal data)
         {
@@ -119,7 +119,7 @@ namespace H6Game.Base
 
     public sealed class MyChar : Value<char>
     {
-        protected static MyChar Instace { get; } = new MyChar();
+        private static MyChar Instace { get; } = new MyChar();
 
         public static explicit operator MyChar(char data)
         {
@@ -135,7 +135,7 @@ namespace H6Game.Base
 
     public sealed class MyByte : Value<byte>
     {
-        protected static MyByte Instace { get; } = new MyByte();
+        private static MyByte Instace { get; } = new MyByte();
 
         public static explicit operator MyByte(byte data)
         {
@@ -151,7 +151,7 @@ namespace H6Game.Base
 
     public sealed class MySByte : Value<sbyte>
     {
-        protected static MySByte Instace { get; } = new MySByte();
+        private static MySByte Instace { get; } = new MySByte();
 
         public static explicit operator MySByte(sbyte data)
         {
@@ -167,7 +167,7 @@ namespace H6Game.Base
 
     public sealed class MyShort : Value<short>
     {
-        protected static MyShort Instace { get; } = new MyShort();
+        private static MyShort Instace { get; } = new MyShort();
 
         public static explicit operator MyShort(short data)
         {
@@ -183,7 +183,7 @@ namespace H6Game.Base
 
     public sealed class MyUShort : Value<ushort>
     {
-        protected static MyUShort Instace { get; } = new MyUShort();
+        private static MyUShort Instace { get; } = new MyUShort();
 
         public static explicit operator MyUShort(ushort data)
         {
@@ -199,7 +199,7 @@ namespace H6Game.Base
 
     public sealed class MyString : Value<string>
     {
-        protected static MyString Instace { get; } = new MyString();
+        private static MyString Instace { get; } = new MyString();
 
         public static explicit operator MyString(string data)
         {
@@ -215,7 +215,7 @@ namespace H6Game.Base
 
     public sealed class MyBoolean : Value<bool>
     {
-        protected static MyBoolean Instace { get; } = new MyBoolean();
+        private static MyBoolean Instace { get; } = new MyBoolean();
 
         public static explicit operator MyBoolean(bool data)
         {
@@ -231,7 +231,7 @@ namespace H6Game.Base
 
     public sealed class MyGuid : Value<Guid>
     {
-        protected static MyGuid Instace { get; } = new MyGuid();
+        private static MyGuid Instace { get; } = new MyGuid();
 
         public static explicit operator MyGuid(Guid data)
         {
@@ -247,7 +247,7 @@ namespace H6Game.Base
 
     public sealed class MyDateTime : Value<DateTime>
     {
-        protected static MyDateTime Instace { get; } = new MyDateTime();
+        private static MyDateTime Instace { get; } = new MyDateTime();
 
         public static explicit operator MyDateTime(DateTime data)
         {
