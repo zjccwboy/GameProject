aec41c81992c764189b966b2cae0ac6513a11abd
sam <70380130@qq.com>
2018-10-07 12:56:10 +0700

    删除非dot net.core测试工程

--- H6Game/H6Game.Test/H6Game.TestDistributed/Close.bat
+++
@@ -1,1 +0,0 @@
-taskkill /F /IM H6Game.TestDistributed.exe
--- H6Game/H6Game.Test/H6Game.TestDistributed/Start2.bat
+++
@@ -1,9 +0,0 @@
-d:
-cd D:\Project\GameProject\H6Game\H6Game.Test\H6Game.TestDistributed\bin\Center\ 
-start D:\Project\GameProject\H6Game\H6Game.Test\H6Game.TestDistributed\bin\Center\H6Game.TestDistributed.exe
-d:
-cd D:\Project\GameProject\H6Game\H6Game.Test\H6Game.TestDistributed\bin\Debug\
-start D:\Project\GameProject\H6Game\H6Game.Test\H6Game.TestDistributed\bin\Debug\H6Game.TestDistributed.exe
-d:
-cd D:\Project\GameProject\H6Game\H6Game.Test\H6Game.TestDistributed\bin\Server2\
-start D:\Project\GameProject\H6Game\H6Game.Test\H6Game.TestDistributed\bin\Server2\H6Game.TestDistributed.exe
